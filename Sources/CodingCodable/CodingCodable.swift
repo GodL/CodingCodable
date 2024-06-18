81f5e6abf1212c38d9b9af14629acecac2971a38
GodL <547188371@qq.com>
2024-06-18 19:39:54 +0800

    support sequence

--- Sources/CodingCodable/CodingCodable.swift
+++ Sources/CodingCodable/CodingCodable.swift
@@ -3,12 +3,12 @@
 
 import Foundation
 
+public enum Error: Swift.Error {
+    case unarchiveFailed
+}
+
 @propertyWrapper
 public struct CodingCodable<T: NSObject&NSCoding> {
-    
-    public enum Error: Swift.Error {
-        case unarchiveFailed
-    }
     
     public var wrappedValue: T
 
@@ -32,5 +32,35 @@ extension CodingCodable : Codable {
         var container = encoder.singleValueContainer()
         let data = try NSKeyedArchiver.archivedData(withRootObject: wrappedValue, requiringSecureCoding: true)
         try container.encode(data)
+    }
+}
+
+public struct CodingArrayCodable<T: NSObject&NSCoding> {
+    public var wrappedValue: [T]
+
+    public init(wrappedValue: [T]) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+extension CodingArrayCodable: Codable {
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let datas = try container.decode([Data].self)
+        self.wrappedValue = try datas.map { data in
+            if let value = try NSKeyedUnarchiver.unarchivedObject(ofClass: T.self, from: data) {
+                return value
+            }else {
+                throw Error.unarchiveFailed
+            }
+        }
+    }
+    
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        let datas = try wrappedValue.map { coding in
+            try NSKeyedArchiver.archivedData(withRootObject: coding, requiringSecureCoding: true)
+        }
+        try container.encode(datas)
     }
 }
